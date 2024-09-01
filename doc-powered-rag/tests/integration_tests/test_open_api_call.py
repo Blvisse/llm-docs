0f79129d680570f480e9a401714992672d20b05e
blaisepapa <blaisepke@gmail.com>
2024-09-01 14:05:28 +0200

    Implementing simple Search Engine

--- doc-powered-rag/tests/integration_tests/test_open_api_call.py
+++ doc-powered-rag/tests/integration_tests/test_open_api_call.py
@@ -3,17 +3,19 @@ from dotenv import load_dotenv
 
 load_dotenv()
 from openai import OpenAI
-from pytest import skip
+from unittest import skip
 
 
-# @skip("Generates real costs")
-# def test_open_api_response():
-#     open_api_key = os.getenv("OPEN_API_KEY")
-#     openai_client = OpenAI(api_key=open_api_key)
-#     response = openai_client.chat.completions.create(
-#         model="gpt-3.5-turbo",
-#         messages=[{"role": "user",
-#                    "content": "write a small hello world poem"}]
-#     )
-#     assert "hello world" in response.choices[0].message.content
-#     assert open_api_key is not None
+@skip("Generates real costs")
+def test_open_api_response():
+    open_api_key = os.getenv("OPEN_API_KEY")
+    openai_client = OpenAI(api_key=open_api_key)
+    response = openai_client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user",
+                   "content": "write a small hello world poem"}]
+    )
+    assert "hello world" in response.choices[0].message.content
+    assert open_api_key is not None
+
+
